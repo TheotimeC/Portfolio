170bbd03dd9410fad7f44f8af162203dfc39dc3d
TheotimeC <theotime.colinet@gmail.com>
2026-05-17 19:08:14 +0200

    correc

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@google/adk"],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
